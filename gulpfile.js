c195ae6582ca38a38698b2b458b4442aca903df0
Roger King <rogr.king@gmail.com>
2017-05-02 00:56:28 -0400

    base automation and development server setup

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const gulp = require('gulp'),
+    webpack = require('webpack'),
+    path = require('path'),
+    rename = require('gulp-rename'),
+    serve = require('browser-sync'),
+    eslint = require('gulp-eslint'),
+    webpackDevMiddelware = require('webpack-dev-middleware'),
+    webpachHotMiddelware = require('webpack-hot-middleware'),
+    colorsSupported = require('supports-color'),
+    config = require('config'),
+    historyApiFallback = require('connect-history-api-fallback');
+
+let root = 'src';
+let resolveToApp = function resolveToApp() {
+    let glob = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+
+    return path.join(root, 'app', glob); // app/{glob}
+};
+
+let resolveToComponents = function resolveToComponents() {
+    let glob = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+
+    return path.join(root, 'app/components', glob); // app/components/{glob}
+};
+
+let paths = {
+    js: resolveToComponents('**/*!(.spec.js).js'), // exclude spec files
+    styl: resolveToApp('**/*.scss'), // stylesheets
+    html: [resolveToApp('**/*.html'), path.join(root, 'index.html')],
+    entry: path.join(__dirname, root, 'index.js'),
+    output: root,
+    blankTemplates: path.join(__dirname, 'generator', 'component/**/*.**')
+};
+
+gulp.task('serve', function () {
+    let config = require('./tools/webpack/webpack.dev.config');
+
+    config.entry.app = [
+        // this modules required to make HRM working
+        // it responsible for all this webpack magic
+        'webpack-hot-middleware/client?reload=true',
+        // application entry point
+        paths.entry
+    ];
+
+    let compiler = webpack(config);
+
+    serve({
+        port: process.env.PORT || 8000,
+        open: false,
+        server: {baseDir: root},
+        ghostMode: false,
+        middleware: [historyApiFallback(), webpackDevMiddelware(compiler, {
+            stats: {
+                colors: colorsSupported,
+                chunks: false,
+                modules: false
+            },
+            publicPath: config.output.publicPath
+        }), webpachHotMiddelware(compiler)]
+    });
+});
+
+gulp.task('watch', ['serve']);
+
+gulp.task('default', ['serve']);
---
+++ tools/webpack/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: "./index.js"
+    },
+    output: {},
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: 'body',
+            hash: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module, count) {
+                return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1;
+            }
+        })
+    ],
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".jsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.js$/, loader: 'babel-loader', exclude: [/node_modules/]},
+            
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    }
+};
