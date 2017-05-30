1046d0045c786f81089e67e6a6c6f034d8732215
Roger King <rogr.king@gmail.com>
2017-05-30 10:40:33 -0400

    fix(service): store using service

--- src/app/components/todo/todoList.component.tsx
+++ src/app/components/todo/todoList.component.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import {inject, observer} from 'mobx-react';
+import * as _ from 'lodash';
 import {TodoItem} from './todoItem.component';
+import {TodosStore} from './../../services/stores';
 
 @inject('todos')
 @observer
 export class TodoListComponent extends React.Component<any, any>{
+
+    componentWillMount(){
+        console.log(this.props.todos.todos.length);
+    }
+
+    componentWillReact(){
+        console.log('Num of todos', this.props.todos.todos.length);
+    }
+
     render(){
         const {todos} = this.props;
-        console.log('Todos', todos.todos.length);
         if(todos.todos.length === 0 ) 
             return (
                 <h1>0 to do.</h1>
--- src/app/services/todos/todos.store.ts
+++ src/app/services/todos/todos.store.ts
@@ -1,14 +1,27 @@
 import { observable, useStrict, action, computed} from 'mobx';
+import * as _ from 'lodash';
+import {TodosService} from './todos.service';
 import {TodoModel as Todo} from './todo.model';
 
 useStrict(true);
 export class TodosStore {
     @observable todos = [];
 
+    constructor(){
+        const parent = this;
+        let service = new TodosService();
+        let remoteTodos = service.getTodos();
+        _.forEach(remoteTodos, function(value, key){
+            parent.todos.push(new Todo(this, value.id, value.title, false));
+        })
+
+    }
+
     @action
     create(title: string){
         const id: number = this.todos.length + 1;
         this.todos.push(new Todo(this, id, title, false));
+        console.log('New Todos', this.todos);
     }
 
     @action
