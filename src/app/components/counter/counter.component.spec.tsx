ded236a432a217c8e98330cb4806f688d4d39130
Roger King <rogr.king@gmail.com>
2017-06-12 16:47:55 -0400

    feat(counter-test): create basic counter test with importing store

--- src/app/components/counter/counter.component.spec.tsx
+++ src/app/components/counter/counter.component.spec.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import {shallow, render} from 'enzyme';
 import * as TestUtils from 'react-addons-test-utils';
-import {expect} from 'chai';
+import * as ReactTestUtils from 'react-dom/test-utils';
+import {expect, assert} from 'chai';
 import {CounterComponent as Counter} from './counter.component';
+import {CounterStore} from './../../services/stores';
 
-describe("Counter Component", function(){
-    let renderer: React.ShallowRenderer;
+const store = {
+    counter: new CounterStore()
+};
 
-    beforeEach(() => {
-        renderer = TestUtils.createRenderer();
-        renderer.render(<Counter/>);
+describe("<Counter/>", function(){
+    let wrapper;
+
+    beforeEach(function() {
+        wrapper = shallow(<Counter counter_store={store.counter}/>)
     });
 
     it("should render correctly", () => {
-        const result = renderer.getRenderOutput();
-        chai.assert.strictEqual(result.type, "div");
+        assert(wrapper.type, "div");
     });
 })
