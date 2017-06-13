d2c788ddecfa88646fd656f0bfd38e7d49f0967b
Roger King <rogr.king@gmail.com>
2017-06-13 16:39:38 -0400

    fix(template): proper case of import name

--- tools/templates/component/component.template.spec.tsx
+++ tools/templates/component/component.template.spec.tsx
@@ -4,7 +4,7 @@ import {mount, render} from 'enzyme';
 import * as TestUtils from 'react-addons-test-utils';
 import * as ReactTestUtils from 'react-dom/test-utils';
 import {expect, assert} from 'chai';
-import { {{pascalCase name}}Component as {{pascalCase name}} } from './{{pascalCase name}}.component';
+import { {{pascalCase name}}Component as {{pascalCase name}} } from './{{lowerCase name}}.component';
 import {CounterStore} from './../../services/stores';
 
 const store = {
