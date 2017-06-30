79b3109f6e869725a8f96db73b83fa4c1ddd74ce
Roger King <rogr.king@gmail.com>
2017-06-29 20:21:11 -0400

    enhancement(typedocs): add templated typedocs

---
+++ src/app/containers/home/home.container.tsx
@@ -0,0 +1,13 @@
+import './home.container.scss';
+import * as React from 'react';
+
+
+export class HomeContainer extends React.Component<any, any> {
+    render() {
+        return(
+            <div>
+                <h1> Hello </h1>
+            </div>
+        );
+    }
+}
---
+++ src/app/containers/home/home.state.ts
@@ -0,0 +1,7 @@
+import { HomeContainer as Home } from './home.container';
+
+export const HomeState = {
+    name: 'home',
+    url: '/',
+    component: Home
+};
