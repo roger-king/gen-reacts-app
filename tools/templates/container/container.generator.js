801bbbb8075c34ce233a7b3daeea118a72e76357
Roger King <rogr.king@gmail.com>
2017-07-25 21:17:33 -0400

    fix(tools): importing container on generation

--- tools/templates/container/container.generator.js
+++ tools/templates/container/container.generator.js
@@ -2,84 +2,85 @@ module.exports = (plop) => {
     plop.setGenerator("container", {
         description: "Create new container (page).",
         prompts: [{
-            type: "input",
-            name: "name",
-            message: "What is the name of your container?"
-        },
-        {
-            type: "confirm",
-            name: "withStore",
-            message: "Create store with container?"
-        },
-        {
-            type: "input",
-            name: "route",
-            message: "What is the route path?"
-        }
+                type: "input",
+                name: "name",
+                message: "What is the name of your container?"
+            },
+            {
+                type: "confirm",
+                name: "withStore",
+                message: "Create store with container?"
+            },
+            {
+                type: "input",
+                name: "route",
+                message: "What is the route path?"
+            }
         ],
-        actions: function (data) {
+        actions: function(data) {
             var actions = [{
-                type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
-                templateFile: "tools/templates/container/container.tsx.tpl"
-            },
-            {
-                type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
-                templateFile: "tools/templates/container/container.spec.tsx.tpl"
-            },
-            {
-                type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.scss",
-                templateFile: "tools/templates/container/container.scss.tpl"
-            },
-            {
-                type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.state.ts",
-                templateFile: "tools/templates/container/container.state.ts.tpl"
-            },
-            {
-                type: "modify",
-                path: "src/app/containers/index.ts",
-                pattern: "// Global imports of all containers (do not remove - will break automation!)",
-                template: "// Global imports of all containers (do not remove - will break automation!)\nimport { {{pascalCase name}}State as {{pascalCase name}} } from './{{camelCase name}}/{{camelCase name}}.state';"
-            }, {
-                type: "modify",
-                path: "src/app/containers/index.ts",
-                pattern: ", ];",
-                template: ", {{pascalCase name}}];"
-            },
-            {
-                type: "modify",
-                path: "src/app/containers/index.ts",
-                pattern: "export const containers = [];",
-                template: "export const containers = [{{pascalCase name}}];"
-            }];
+                    type: "add",
+                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
+                    templateFile: "tools/templates/container/container.tsx.tpl"
+                },
+                {
+                    type: "add",
+                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
+                    templateFile: "tools/templates/container/container.spec.tsx.tpl"
+                },
+                {
+                    type: "add",
+                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.scss",
+                    templateFile: "tools/templates/container/container.scss.tpl"
+                },
+                {
+                    type: "add",
+                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.state.ts",
+                    templateFile: "tools/templates/container/container.state.ts.tpl"
+                },
+                {
+                    type: "modify",
+                    path: "src/app/containers/index.ts",
+                    pattern: "// Global imports of all containers (do not remove - will break automation!)",
+                    template: "// Global imports of all containers (do not remove - will break automation!)\nimport { {{pascalCase name}}State } from './{{camelCase name}}/{{camelCase name}}.state';"
+                }, {
+                    type: "modify",
+                    path: "src/app/containers/index.ts",
+                    pattern: "State];",
+                    template: "State, {{pascalCase name}}State];"
+                },
+                {
+                    type: "modify",
+                    path: "src/app/containers/index.ts",
+                    pattern: "export const containers = [];",
+                    template: "export const containers = [{{pascalCase name}}State];"
+                }
+            ];
 
             if (data.withStore) {
                 actions = actions.concat([{
-                    type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
-                    pattern: "export class ",
-                    template: "@inject('{{snakeCase name}}_store')\n@observer\nexport class "
-                }, {
-                    type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
-                    pattern: "import * as React from 'react';",
-                    template: "import * as React from 'react';\nimport {inject, observer} from 'mobx-react';"
-                },
-                {
-                    type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
-                    pattern: ".container';",
-                    template: ".container';\nimport { {{pascalCase name}}Store } from './../../services/stores';\nconst store = {\n {{snakeCase name}}: new {{pascalCase name}}()\n};\n"
-                },
-                {
-                    type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
-                    pattern: /mount\((.*)\)/g,
-                    template: "mount(<{{pascalCase name}} {{snakeCase name}}_store={ store.{{camelCase name}} }/>)"
-                }
+                        type: "modify",
+                        path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
+                        pattern: "export class ",
+                        template: "@inject('{{snakeCase name}}_store')\n@observer\nexport class "
+                    }, {
+                        type: "modify",
+                        path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
+                        pattern: "import * as React from 'react';",
+                        template: "import * as React from 'react';\nimport {inject, observer} from 'mobx-react';"
+                    },
+                    {
+                        type: "modify",
+                        path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
+                        pattern: ".container';",
+                        template: ".container';\nimport { {{pascalCase name}}Store } from './../../services/stores';\nconst store = {\n {{snakeCase name}}: new {{pascalCase name}}()\n};\n"
+                    },
+                    {
+                        type: "modify",
+                        path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
+                        pattern: /mount\((.*)\)/g,
+                        template: "mount(<{{pascalCase name}} {{snakeCase name}}_store={ store.{{camelCase name}} }/>)"
+                    }
                 ])
             }
 
