5899d85beea6c5e6325c44dfef5414b51919f5ac
Roger King <rogr.king@gmail.com>
2017-08-01 15:29:36 -0400

    fix(linting): linting errors in templates

--- src/app/app.router.tsx
+++ src/app/app.router.tsx
@@ -1,39 +1,6 @@
 import * as React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import routes from './app.routes';
-
-/**
- * ProtectedRoute
- * 
- * This is a wrapper component protected routes behind an auth.
- * In app.routes file, add the key auth: true and the component
- * looks for that boolean value.
- */
-
-/* export class ProtectedRoute extends React.Component<any, any> {
-
-    isAuthenticated() {
-        return isAuthenticated();
-    }
-
-    render() {
-        const { route, history } = this.props;
-
-        return (
-            <Route exact={route.exact} path={route.path} render={
-                props => (
-                    this.isAuthenticated() ? (
-                        <route.component {...props } routes={route.routes} />
-                    ) : (
-                            <Redirect to={{
-                                pathname: '/',
-                                state: { from: props.location }
-                            }} />
-                        )
-                )} />
-        );
-    }
-} */
+import routes from './containers';
 
 export class PublicRoute extends React.Component<any, any> {
 
--- tools/templates/container/container.route.ts.tpl
+++ tools/templates/container/container.route.ts.tpl
@@ -1,7 +1,7 @@
-import { {{pascalCase name}}Container as {{pascalCase name}}ard } from './{{camelCase name}}.container';
+import { {{pascalCase name}}Container as {{pascalCase name}} } from './{{camelCase name}}.container';
 
-export const {{pascalCase name}}Route= {
+export const {{pascalCase name}}Route = {
     exact: false,
-    path: {{route}}
+    path: '{{route}}',
     component: {{pascalCase name}}
-}
+};
