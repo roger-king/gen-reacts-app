1febdc578ca464021cbb6afc796e54a432785f54
Roger King <rking@zillion.com>
2017-11-20 17:29:49 -0500

    wip: added react logo

--- src/app/components/titletron/titletron.component.tsx
+++ src/app/components/titletron/titletron.component.tsx
@@ -8,6 +8,7 @@ import * as React from 'react';
 export const Titletron: React.SFC<any> = (props) => {
     return(
         <div className="titletron-container">
+        <img src="https://availity.github.io/react-block-ui/assets/logo.png"/>
             <span className="title"> React Typescript Boilerplate</span>
         </div>
     );
