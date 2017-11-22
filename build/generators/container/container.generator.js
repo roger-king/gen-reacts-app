deec95992e987c338d2c65bf46ed27b34a88e6e5
Roger King <rking@zillion.com>
2017-11-22 10:49:36 -0500

    fix: path issue

--- build/generators/container/container.generator.js
+++ build/generators/container/container.generator.js
@@ -20,39 +20,39 @@ module.exports = (plop) => {
         actions: function (data) {
             var actions = [{
                 type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
-                templateFile: "build/templates/container/container.tsx.tpl"
+                path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
+                templateFile: "container/container.tsx.tpl"
             },
             {
                 type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.test.tsx",
-                templateFile: "build/templates/container/container.test.tsx.tpl"
+                path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.container.test.tsx",
+                templateFile: "container/container.test.tsx.tpl"
             },
             {
                 type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.css",
-                templateFile: "build/templates/container/container.css.tpl"
+                path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.container.css",
+                templateFile: "container/container.css.tpl"
             },
             {
                 type: "add",
-                path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.route.ts",
-                templateFile: "build/templates/container/container.route.ts.tpl"
+                path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.route.ts",
+                templateFile: "container/container.route.ts.tpl"
             },
             {
                 type: "modify",
-                path: "src/app/containers/index.ts",
+                path: "../../src/app/containers/index.ts",
                 pattern: "// Global imports of all containers (do not remove - will break automation!)",
                 template: "// Global imports of all containers (do not remove - will break automation!)\nimport { {{pascalCase name}}Route } from './{{camelCase name}}/{{camelCase name}}.route';"
             },
             {
                 type: "modify",
-                path: "src/app/containers/index.ts",
+                path: "../../src/app/containers/index.ts",
                 pattern: "];",
                 template: ", {{pascalCase name}}Route];"
             },
             {
                 type: "modify",
-                path: "src/app/containers/index.ts",
+                path: "../../src/app/containers/index.ts",
                 pattern: "// export default []",
                 template: "export default [{{pascalCase name}}Route];"
             }
@@ -61,24 +61,24 @@ module.exports = (plop) => {
             if (data.withStore) {
                 actions = actions.concat([{
                     type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
+                    path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
                     pattern: "export class ",
                     template: "@inject('{{snakeCase name}}_store')\n@observer\nexport class "
                 }, {
                     type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
+                    path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.container.tsx",
                     pattern: "import * as React from 'react';",
                     template: "import * as React from 'react';\nimport {inject, observer} from 'mobx-react';"
                 },
                 {
                     type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
+                    path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
                     pattern: ".container';",
                     template: ".container';\nimport { {{pascalCase name}}Store } from './../../services/stores';\nconst store = {\n {{snakeCase name}}: new {{pascalCase name}}()\n};\n"
                 },
                 {
                     type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
+                    path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.container.spec.tsx",
                     pattern: /mount\((.*)\)/g,
                     template: "mount(<{{pascalCase name}} {{snakeCase name}}_store={ store.{{camelCase name}} }/>)"
                 }
@@ -88,7 +88,7 @@ module.exports = (plop) => {
             if (data.route === "/") {
                 actions = actions.concat([{
                     type: "modify",
-                    path: "src/app/containers/{{camelCase name}}/{{camelCase name}}.route.ts",
+                    path: "../../src/app/containers/{{camelCase name}}/{{camelCase name}}.route.ts",
                     pattern: "exact: false,",
                     template: "exact: true,"
                 }])
