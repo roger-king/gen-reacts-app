abac8b574c3f7f044b183ae25141ad9e2cf60b35
roger-king <rogr.king@gmail.com>
2017-11-23 14:15:20 -0500

    chore(state): adding import comment

--- src/app/services/states.ts
+++ src/app/services/states.ts
@@ -1,3 +1,5 @@
+// Import INITIAL_STATE of our services
+
 import {INITIAL_STATE as UserState} from './user/user.reducer';
 import {INITIAL_STATE as AuthenticationState} from './authentication/authentication.reducer';
 
