d0cd8bcae5c300d8225fc49e95cae42ef378e830
roger-king <rogr.king@gmail.com>
2017-11-24 10:04:33 -0500

    fix(titletron): proper lost grid

--- src/app/components/titletron/titletron.component.tsx
+++ src/app/components/titletron/titletron.component.tsx
@@ -9,16 +9,16 @@ import { Icon } from './../';
 export const Titletron: React.SFC<any> = (props) => {
     return (
         <section className="titletron-container">
+            <div className="titletron-wrapper">
+                <Icon img="react-1.svg" height={100} width={100} />
+                <span className="title titletron-title"> Typescript Kit</span>
+            </div>
             <div className="icons-wrapper">
                 <Icon img="webpack.svg" height={40} width={40} />
                 <Icon img="redux.svg" height={40} width={40} />
                 <Icon img="react-router.svg" height={40} width={40} />
                 <Icon img="postcss.svg" height={40} width={40} />
                 <Icon img="jest.svg" height={40} width={40}/>
-            </div>
-            <div className="titletron-wrapper">
-                <Icon img="react-1.svg" height={100} width={100} />
-                <span className="title titletron-title"> Typescript Kit</span>
             </div>
         </section>
     );
