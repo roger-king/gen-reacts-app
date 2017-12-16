184de611f241993c2014cf2abdb238cb593be5f9
roger-king <rogr.king@gmail.com>
2017-12-16 13:12:42 -0500

    feat(code): creating code component to display code snippets

---
+++ src/app/components/code/code.component.tsx
@@ -0,0 +1,22 @@
+import './code.component.css';
+import * as React from 'react';
+
+/**
+ * Code Component
+ */
+
+interface ICode {
+    codeline: string;
+    description: string;
+}
+
+export const Code: React.SFC<any> = (props: ICode) => {
+    return(
+        <div className="codeline-container">
+            <span className="description"> {props.description} </span>
+            <div className="codeline">
+                $ {props.codeline}
+            </div>
+        </div>
+    );
+};
