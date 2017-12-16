b230c3db77f1d81c1d62d5701d922937f43de1c3
roger-king <rogr.king@gmail.com>
2017-12-16 13:16:53 -0500

    feat: add new components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,6 +4,9 @@
  */
 
 // Global imports of all components (do not remove - will break automation!)
+export { Code } from './code/code.component';
+export { Headline } from './headline/headline.component';
+export { Footer } from './footer/footer.component';
 export { Icon } from './icon/icon.component';
 export { Titletron } from './titletron/titletron.component';
-export { ConnectedLogin as Login  } from './login/login.component';
+export { ConnectedLogin as Login } from './login/login.component';
