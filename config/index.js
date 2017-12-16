222ae9401788a43d1c15f69e1eab43952ea2b482
roger-king <rogr.king@gmail.com>
2017-12-16 18:56:27 -0500

    static assets

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'react-typescript-gen/static',
     assetsPublicPath: '/assets',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
