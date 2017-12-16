1d81ebe1f544f0cb73d8ee4d40c8427ece0fb945
roger-king <rogr.king@gmail.com>
2017-12-16 13:35:20 -0500

    feat(unit-test): create ui unit test

---
+++ __test__/app/components/code.component.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Code } from './../../../src/app/components';
+
+let tree;
+
+beforeEach(function () {
+    tree = renderer.create(
+        <Code description="present working directory" codeline="pwd"/>
+    );
+});
+
+describe('<Code/>', function () {
+    test('renders properly', () => {
+        let component = tree.toJSON();
+        expect(component.children.length).toEqual(2);
+        expect(component.props.className).toEqual('codeline-container');
+    });
+
+    test('Code nodes', () => {
+        let component = tree.toJSON();
+        expect(component.children[0].children[1]).toEqual('present working directory');
+        expect(component.children[1].children[0]).toEqual('$ ');
+        expect(component.children[1].children[1]).toEqual('pwd');
+    });
+});
