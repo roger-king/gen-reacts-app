aaf85bec84cadfe2c259f18bf760c5c7c0b0a44b
roger-king <rogr.king@gmail.com>
2017-12-16 13:40:57 -0500

    feat(unittest): creating footer unit test

---
+++ __test__/app/components/footer.component.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Footer } from './../../../src/app/components';
+
+let tree;
+
+beforeEach(function () {
+    tree = renderer.create(
+        <Footer/>
+    );
+});
+
+describe('<Footer/>', function () {
+    test('renders properly', () => {
+        let component = tree.toJSON();
+        expect(component.children[0].type).toEqual('a');
+        expect(component.children[0].props.href).toEqual('https://github.com/roger-king/react-typescript-gen');
+        expect(component.children[0].props.target).toEqual('blank');
+        expect(component.children[0].children[0].type).toEqual('img');
+        expect(component.children[0].children[0].props.src).toEqual('static/images/github.png');
+        expect(component.children[0].children[0].props.width).toEqual(50);
+        expect(component.children[0].children[0].props.height).toEqual(50);
+    });
+});
