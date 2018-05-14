f1e3f3605ed6291119722b94a6abcdfa0525747b
Roger King <rogr.king@gmail.com>
2018-05-13 20:22:24 -0400

    fix(path): hoc path

--- config/generators/plopfile.js
+++ config/generators/plopfile.js
@@ -1,37 +1,39 @@
 var component = require('./component/component.generator');
 var page = require('./page/page.generator');
-var hoc = require('./hoc/service.generator');
+var hoc = require('./hoc/hoc.generator');
 
-
-module.exports = (plop) => {
+module.exports = plop => {
     // plop.setPrompt('recursive', require('inquirer-recursive'));
     plop.setGenerator('init', {
-        description: "Initial run command to start project",
-        prompts: [{
-            type: "input",
-            name: "title",
-            message: "What is the name of your project?"
-        }],
+        description: 'Initial run command to start project',
+        prompts: [
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the name of your project?',
+            },
+        ],
         actions: [
             {
-                type: "modify",
-                path: "src/index.html",
-                pattern: "React.js-Modern-Web-Seed",
-                template: "{{title}}"
-            }, function(data){
+                type: 'modify',
+                path: 'src/index.html',
+                pattern: 'React.js-Modern-Web-Seed',
+                template: '{{title}}',
+            },
+            function(data) {
                 let fs = require('fs');
 
-                if(fs.existsSync('./.git')){
+                if (fs.existsSync('./.git')) {
                     fs.rmdir('./.git', () => {
                         console.log('REMOVE THE .git Folder');
                         console.log('Ready to start your new react project!');
-                        console.log('Try running \'npm run generate\' ');
+                        console.log("Try running 'npm run generate' ");
                     });
                 }
-            }
-        ]
+            },
+        ],
     });
     component(plop);
     page(plop);
     hoc(plop);
-}
+};
