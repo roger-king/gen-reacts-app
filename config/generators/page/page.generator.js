50fe3260690f03242c3c64df7690535108ecacf0
Roger King <rogr.king@gmail.com>
2018-05-13 20:23:41 -0400

    feat: complete page generator

--- config/generators/page/page.generator.js
+++ config/generators/page/page.generator.js
@@ -1,4 +1,7 @@
 var appModuleExists = require('./../../utils').appModuleExists;
+var fs = require('fs');
+var path = require('path');
+var readLine = require('readline');
 
 module.exports = plop => {
     plop.setGenerator('page', {
@@ -16,8 +19,14 @@ module.exports = plop => {
                     return 'The title is required';
                 },
             },
+            {
+                type: 'input',
+                name: 'uri',
+                message: 'URI',
+            },
         ],
         actions: function(data) {
+            var actions = [];
             var actions = [
                 {
                     type: 'add',
@@ -26,7 +35,7 @@ module.exports = plop => {
                 },
                 {
                     type: 'add',
-                    path: './../../src/app/pages/{{camelCase name}}.tsx',
+                    path: './../../src/app/pages/{{camelCase name}}Page.tsx',
                     templateFile: 'page/pageComponent.tsx.tpl',
                 },
             ];
@@ -39,13 +48,49 @@ module.exports = plop => {
                     template:
                         "import * as Loadable from 'react-loadable';\n\n" +
                         'export const Loadable{{pascalCase name}} = Loadable({\n' +
-                        "    loader: () => import('./{{camelCase name}}'),\n" +
+                        "    loader: () => import('./{{camelCase name}}Page'),\n" +
                         '    loading() {\n' +
                         '        return <div> loading... </div>;\n' +
                         '    },\n' +
-                        '});\n',
+                        '});',
+                },
+
+                // Linting error will occur on the component because of how plop will parsefor the name variable
+                {
+                    type: 'modify',
+                    path: './../../src/app/app.router.tsx',
+                    pattern: '        <Switch>',
+                    template:
+                        '        <Switch>\n' +
+                        '            <Route path="{{uri}}" component={ Loadable{{pascalCase name}} } />\n',
                 },
             ]);
+
+            const lineReader = readLine.createInterface({
+                input: fs.createReadStream(path.resolve(__dirname, '../../../src/app/app.router.tsx')),
+            });
+            lineReader.on('line', line => {
+                if (line.includes("} from './pages';")) {
+                    actions = actions.concat([
+                        {
+                            type: 'modify',
+                            path: './../../src/app/app.router.tsx',
+                            pattern: "} from './pages';",
+                            template: ", Loadable{{pascalCase name}} } from './pages';",
+                        },
+                    ]);
+                } else {
+                    actions = actions.concat([
+                        {
+                            type: 'modify',
+                            path: './../../src/app/app.router.tsx',
+                            pattern: "import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';",
+                            template:
+                                "import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';\nimport { Loadable{{pascalCase name}} } from './pages';",
+                        },
+                    ]);
+                }
+            });
 
             return actions;
         },
