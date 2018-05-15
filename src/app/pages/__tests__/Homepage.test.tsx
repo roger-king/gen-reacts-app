2eb39ed80c839613a9e8739c6b2f8ac0d77e3db2
Roger King <rogr.king@gmail.com>
2018-05-14 23:27:48 -0400

    chore(format): change code format to 2 tabWidth

--- src/app/pages/__tests__/Homepage.test.tsx
+++ src/app/pages/__tests__/Homepage.test.tsx
@@ -3,6 +3,6 @@ import { create } from 'react-test-renderer';
 import HomePage from './../HomePage';
 
 test('Snapshot of Home Page', () => {
-    const page = create(<HomePage />).toJSON();
-    expect(page).toMatchSnapshot();
+  const page = create(<HomePage />).toJSON();
+  expect(page).toMatchSnapshot();
 });
--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as Loadable from 'react-loadable';
 
 export const LoadableHomePage = Loadable({
-    loader: () => import('./HomePage'),
-    loading() {
-        return <div> loading...</div>;
-    },
+  loader: () => import('./HomePage'),
+  loading() {
+    return <div> loading...</div>;
+  },
 });
