2f3109d56d931d6bcfdf072bbb05b2ce8cd7b725
Roger King <rogr.king@gmail.com>
2018-05-22 20:53:58 -0400

    write package.json

--- src/utils/writePackageJson.ts
+++ src/utils/writePackageJson.ts
@@ -5,7 +5,7 @@ import { homedir } from 'os';
 export const writePackageJson = (pathToPackageJson: string) => {
     const arrayOfPath = pathToPackageJson.split('/');
     const projectTitle = arrayOfPath[arrayOfPath.length - 1];
-    const basePackageJson = require(path.resolve(pathToPackageJson));
+    const basePackageJson = require(path.join(path.resolve(pathToPackageJson), 'package.json'));
 
     const gitConfigPath = path.resolve(homedir(), '.gitconfig');
     let credentials: { name: string | null; email: string | null } = {
@@ -19,4 +19,15 @@ export const writePackageJson = (pathToPackageJson: string) => {
         credentials['name'] = gitCredentials[0].trim();
         credentials['email'] = gitCredentials[1].trim();
     }
+
+    basePackageJson.name = projectTitle;
+    basePackageJson.description = `${projectTitle} - ReactJS Typescript Single Page Application`;
+    basePackageJson.author = credentials.name ? `${credentials.name}` : '';
+    basePackageJson.author = credentials.email ? `${basePackageJson.author} <${credentials.email}>` : '';
+
+    fs.writeFileSync(
+        path.join(path.resolve(pathToPackageJson), 'package.json'),
+        JSON.stringify(basePackageJson),
+        'utf8',
+    );
 };
