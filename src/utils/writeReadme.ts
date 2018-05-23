1874707cf72ce3980a62c1e6e54d3016686d9c47
Roger King <rogr.king@gmail.com>
2018-05-22 21:59:12 -0400

    move readme

--- src/utils/writeReadme.ts
+++ src/utils/writeReadme.ts
@@ -1,9 +1,14 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as child from 'child_process';
 
-export const writeReadme = (pathToPackageJson: string) => {
-    const arrayOfPath = pathToPackageJson.split('/');
+export const writeReadme = (pathToApp: string) => {
+    const arrayOfPath = pathToApp.split('/');
     const projectTitle = arrayOfPath[arrayOfPath.length - 1];
 
-    fs.writeFileSync(path.join(path.resolve(pathToPackageJson), 'README.md'), `#${projectTitle}`, 'utf8');
+    child.execSync(
+        `mv ${path.join(path.resolve(pathToApp), 'README.md')} ${path.join(path.resolve(pathToApp), 'BASE.md')}`,
+    );
+
+    fs.writeFileSync(path.join(path.resolve(pathToApp), 'README.md'), `#${projectTitle}`, 'utf8');
 };
